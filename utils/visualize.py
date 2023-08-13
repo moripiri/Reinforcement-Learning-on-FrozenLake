230ca99ab775fe70b47d50d09063b60faea60e9b
moripiri <moripiri1234@gmail.com>
2023-08-13 21:26:54 +0900

    plot_v

--- utils/visualize.py
+++ utils/visualize.py
@@ -1,4 +1,4 @@
-import matplotlib as pyplot
+import matplotlib.pyplot as plt
 import numpy as np
 
 def visualize_model(model: np.ndarray) -> None:
@@ -13,3 +13,17 @@ def visualize_v(v_func: np.ndarray) -> None:
 def visualize_policy(policy: np.ndarray) -> None:
     pass
 
+
+def plot_values(V):
+    # reshape value function
+    V_sq = np.reshape(V, (4,4))
+
+    # plot the state-value function
+    fig = plt.figure(figsize=(6, 6))
+    ax = fig.add_subplot(111)
+    im = ax.imshow(V_sq, cmap='cool')
+    for (j,i),label in np.ndenumerate(V_sq):
+        ax.text(i, j, np.round(label, 5), ha='center', va='center', fontsize=14)
+    plt.tick_params(bottom=False, left=False, labelbottom=False, labelleft=False)
+    plt.title('State-Value Function')
+    plt.show()
