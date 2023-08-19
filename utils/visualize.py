a5d59e0e1cc842ddfc6c8efea0d15667436c606e
moripiri <moripiri1234@gmail.com>
2023-08-20 00:04:05 +0900

    plotting

--- utils/visualize.py
+++ utils/visualize.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def visualize_model(model: np.ndarray, nrow=4, ncol=4) -> None:
+def visualize_model(model, ax, nrow=4, ncol=4, title='Model') -> None:
     # visualize model
-    visualize_q(model, nrow, ncol)
+    visualize_q(model, ax, nrow, ncol, title)
 
-def visualize_q(q_func: np.ndarray, nrow=4, ncol=4) -> None:
+def visualize_q(q_func, ax, nrow=4, ncol=4, title='State-Action Value') -> None:
     # visualize state-action value
 
     q_func = q_func.reshape([nrow, ncol, 4])
@@ -14,8 +14,8 @@ def visualize_q(q_func: np.ndarray, nrow=4, ncol=4) -> None:
     v_ticks = np.arange(-0.5, ncol + 0.5, 1)
 
     # plot the state-action function
-    fig = plt.figure(figsize=(8, 8))
-    ax = fig.add_subplot(111)
+    # fig = plt.figure(figsize=(8, 8))
+    # ax = fig.add_subplot(111)
     im = ax.imshow(np.zeros([nrow, ncol]), alpha=0.)
 
     for tick in h_ticks:
@@ -33,21 +33,22 @@ def visualize_q(q_func: np.ndarray, nrow=4, ncol=4) -> None:
 
     for (j, i, k), q in np.ndenumerate(q_func):
         if k == 0:
-            ax.text(i - 0.3, j, np.round(q, 3), ha='center', va='center', fontsize=14)  # left
+            ax.text(i - 0.3, j, np.round(q, 2), ha='center', va='center', fontsize=8)  # left
         elif k == 1:
-            ax.text(i, j + 0.3, np.round(q, 3), ha='center', va='center', fontsize=14)  # bottom
+            ax.text(i, j + 0.3, np.round(q, 2), ha='center', va='center', fontsize=8)  # bottom
         elif k == 2:
-            ax.text(i + 0.3, j, np.round(q, 3), ha='center', va='center', fontsize=14)  # right
+            ax.text(i + 0.3, j, np.round(q, 2), ha='center', va='center', fontsize=8)  # right
         elif k == 3:
-            ax.text(i, j - 0.3, np.round(q, 3), ha='center', va='center', fontsize=14)  # top
+            ax.text(i, j - 0.3, np.round(q, 2), ha='center', va='center', fontsize=8)  # top
         else:
             raise ValueError
+    ax.set_title(title)
 
-    plt.tick_params(bottom=False, left=False, labelbottom=False, labelleft=False)
-    plt.show()
+#     plt.tick_params(bottom=False, left=False, labelbottom=False, labelleft=False)
+#     plt.show()
 
 
-def visualize_v(v_func: np.array, nrow=4, ncol=4) -> None:
+def visualize_v(v_func, ax, nrow=4, ncol=4, title='State Value') -> None:
     # visualize state value
 
     v_func = v_func.reshape([nrow, ncol])
@@ -56,8 +57,8 @@ def visualize_v(v_func: np.array, nrow=4, ncol=4) -> None:
     v_ticks = np.arange(-0.5, ncol + 0.5, 1)
 
     # plot the state-value function
-    fig = plt.figure(figsize=(8, 8))
-    ax = fig.add_subplot(111)
+    # fig = plt.figure(figsize=(8, 8))
+    # ax = fig.add_subplot(111)
     im = ax.imshow(np.zeros([nrow, ncol]), alpha=0.)
 
     for tick in h_ticks:
@@ -67,35 +68,40 @@ def visualize_v(v_func: np.array, nrow=4, ncol=4) -> None:
         ax.axvline(tick, color='black', linewidth=0.3)
 
     for (j, i), value in np.ndenumerate(v_func):
-        ax.text(i, j, np.round(value, 3), ha='center', va='center', fontsize=16)
+        ax.text(i, j, np.round(value, 3), ha='center', va='center', fontsize=14)
+        
+    ax.set_title(title)
+    # plt.tick_params(bottom=False, left=False, labelbottom=False, labelleft=False)
+    # plt.show()
 
-    plt.tick_params(bottom=False, left=False, labelbottom=False, labelleft=False)
-    plt.show()
-
-def visualize_policy(policy: np.ndarray, nrow=4, ncol=4) -> None:
+def visualize_policy(policy, ax, nrow=4, ncol=4, title='Policy') -> None:
     # visualize deterministic policy
-    arrow = {0: '←', 1: '↓', 2: '→', 3: '↑'}
+    if len(policy.shape) == 2:
+        visualize_q(policy, ax, nrow, ncol, title='Stochastic Policy')
+    else:
+        arrow = {0: '←', 1: '↓', 2: '→', 3: '↑'}
 
-    policy = policy.reshape([nrow, ncol])
+        policy = policy.reshape([nrow, ncol])
 
-    h_ticks = np.arange(-0.5, nrow + 0.5, 1)
-    v_ticks = np.arange(-0.5, ncol + 0.5, 1)
+        h_ticks = np.arange(-0.5, nrow + 0.5, 1)
+        v_ticks = np.arange(-0.5, ncol + 0.5, 1)
 
-    fig = plt.figure(figsize=(8, 8))
-    ax = fig.add_subplot(111)
-    im = ax.imshow(np.zeros([nrow, ncol]), alpha=0.)
+        #fig = plt.figure(figsize=(8, 8))
+        #ax = fig.add_subplot(111)
+        im = ax.imshow(np.zeros([nrow, ncol]), alpha=0.)
 
-    for tick in h_ticks:
-        ax.axhline(tick, color='black', linewidth=0.3)
+        for tick in h_ticks:
+            ax.axhline(tick, color='black', linewidth=0.3)
 
-    for tick in v_ticks:
-        ax.axvline(tick, color='black', linewidth=0.3)
+        for tick in v_ticks:
+            ax.axvline(tick, color='black', linewidth=0.3)
 
-    for (j, i), p in np.ndenumerate(policy):
-        ax.text(i, j, arrow[p], ha='center', va='center', fontsize=40)
+        for (j, i), p in np.ndenumerate(policy):
+            ax.text(i, j, arrow[p], ha='center', va='center', fontsize=36)
 
-    plt.tick_params(bottom=False, left=False, labelbottom=False, labelleft=False)
-    plt.show()
+        ax.set_title(title)
+        #plt.tick_params(bottom=False, left=False, labelbottom=False, labelleft=False)
+        #plt.show()
 
 
 
@@ -104,4 +110,3 @@ if __name__ == '__main__':
     print('↑')
     a = np.array([1, 2, 1, 0, 1, 0, 1, 0, 2, 1, 1, 0, 0, 2, 2, 0], dtype=int)
 
-    visualize_policy(a, 4, 4)
--- utils/wrapper.py
+++ utils/wrapper.py
@@ -3,19 +3,59 @@ import matplotlib
 import IPython
 from IPython import display
 import matplotlib.pyplot as plt
+from utils.visualize import visualize_policy, visualize_q, visualize_model, visualize_v
 
 class JupyterRender(gym.Wrapper):
     def __init__(self, env):
         super().__init__(env)
+        self.env = env
 
-    def render(self):
-        img = plt.imshow(self.env.render())  # prepare to render the environment by using matplotlib and ipython display
-        plt.xticks([]) 
-        plt.yticks([]) 
+    def render(self, title='Environment', v=None, q=None, policy=None, model_r=None, model_ns=None):
+        viz_list = {}
+        if v is not None:
+            viz_list['v'] = v
+        if q is not None:
+            viz_list['q'] = q
+        if policy is not None:
+            viz_list['policy'] = policy
+        if model_r is not None:
+            viz_list['model_r'] = model_r
+        if model_ns is not None:
+            viz_list['model_ns'] = model_ns
 
+        fig = plt.figure(figsize=(8, 8))
+
+        ax_list = [fig.add_subplot(2, 2, 1)]
+        img = ax_list[0].imshow(self.env.render())  # prepare to render the environment by using matplotlib and ipython display
+        ax_list[0].set_title(title)
+        
+        pos = 2
+        for i in range(pos, 2 + len(viz_list)):
+            ax_list.append(fig.add_subplot(2, 2, i))
+
+        ax_index = 1
+        for key, value in viz_list.items():
+            if key == 'policy':
+                visualize_policy(value, ax_list[ax_index], self.env.nrow, self.env.ncol)
+            elif key == 'v':
+                visualize_v(value, ax_list[ax_index], self.env.nrow, self.env.ncol)
+            elif key == 'q':
+                visualize_q(value, ax_list[ax_index], self.env.nrow, self.env.ncol)
+            else:
+                if key == 'model_r':
+                    title = 'Reward Model'
+                elif key == 'model_ns':
+                    title = 'Next State Model'
+                visualize_model(value, ax_list[ax_index], self.env.nrow, self.env.ncol, title)
+
+            ax_index += 1
+
+        for ax in ax_list:
+            ax.tick_params(bottom=False, left=False, labelbottom=False, labelleft=False)
+            
         display.display(plt.gcf())
         display.clear_output(wait=True)
-        plt.clf()
+        plt.close()
         
     
 
